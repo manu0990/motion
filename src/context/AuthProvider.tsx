547458150e015bb5dccb8f3820c0123877beed5a
Manu09 <manabda751@gmail.com>
2025-07-20 22:02:23 +0530

    refactor: remove unused form and tabs components and packages;

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -2,12 +2,11 @@
 
 import { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import { RecoilRoot } from 'recoil';
 
 export const AuthProviders = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
-      <RecoilRoot>{children}</RecoilRoot>
+      {children}
     </SessionProvider>
   );
 };
